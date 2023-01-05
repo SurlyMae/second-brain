1a0b2f31c59d798bb4064f6a536d4590c523f605
Ashly LaMarr <surlymae@gmail.com>
2023-01-05 14:56:43 -0700

    2023

--- components/layout.js
+++ components/layout.js
@@ -4,23 +4,21 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = ''
+export const siteTitle = 'Ashly LaMarr'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="second brain"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="/images/Jurgen_tifo.png"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -30,11 +28,12 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/Jurgen_tifo.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
-              alt={name}
+              alt="An artistic representation of LFC gaffer Jurgen Klopp"
+              title='Credit: Tifo Football'
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -44,11 +43,12 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/Jurgen_tifo.png"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
-                  alt={name}
+                  alt="An artistic representation of LFC gaffer Jurgen Klopp"
+                  title='Credit: Tifo Football'
                 />
               </a>
             </Link>
---
+++ posts/bookslist.md
@@ -0,0 +1,10 @@
+---
+title: "2023 books read"
+date: "2023-01-01"
+---
+
+**Shrines of Gaiety by Kate Atkinson**
+
+post WWII in Soho, London. Good story, interesting characters, stellar storytelling. 
+Highly recommend.
+
